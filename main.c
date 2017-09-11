a1a042345de7e9643d4fb4cfa73d7338f64078c1
toshokan <toshokan@shojigate.net>
2017-09-10 21:17:23 -0400

    Allow choosing to view results from a previously saved resource

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 bool write_resource_to_file(char *filename, char *resource);
 int scan_mode(char *apikey, char *filename);
 int result_mode(char *apikey, char *filename);
-int list_pending();
+char *list_pending();
 
 int main(int argc, char *argv[]){
 	char apikey[65];
@@ -46,7 +46,8 @@ int main(int argc, char *argv[]){
 					break;
 				case 'r':
 					if(strcmp(argv[2],"list")==0){
-						list_pending();
+						char *fname = list_pending();
+						return result_mode(apikey, fname);
 					}
 					printf("result mode, asked to check %s\n",argv[2]);
 					return result_mode(apikey, argv[2]);
@@ -164,27 +165,45 @@ int result_mode(char *apikey, char *filename){
 	free(slist);
 }
 
-int list_pending(){
+char *list_pending(){
 	DIR *d = opendir("vpending");
 	struct dirent *dent;
 	if(!d){
 		fprintf(stderr, "Unable to open the \"vpending\" directory.");
-		return EXIT_FAILURE;
+		return NULL;
 	}
+	int current_file = 0; 
+	struct dirent *files[10];
+	printf("Here are the previous scans you might want to check on: \n");
 	while(dent = readdir(d)){
 		if(dent->d_type == DT_REG){
 			char path[256];
 			sprintf(path,"vpending/%s",dent->d_name);
-			char contents[1024]; 
-			FILE *fd = fopen(path,"r");
+			//char contents[1024]; 
+			//FILE *fd = fopen(path,"r");
 			// FIXME
 			// Check ptr
-			fgets(contents,1024,fd);
-			printf("%s\n\t%s\n",dent->d_name,contents);
-			contents[0] = '\0';
-			fclose(fd);
+			printf("\t[%i] %s\n",current_file,dent->d_name);
+			//files[current_file] = fd;
+			files[current_file] = dent;
+			current_file++;
+			//fgets(contents,1024,fd);
+			//printf("%s\n\t%s\n",dent->d_name,contents);
+			//contents[0] = '\0';
+			//fclose(fd);
 		}
 	}
+	int selection = -1;
+	while(selection < 0 || selection > current_file){
+		printf("Choose a file to check on by entering its bracketed number: ");
+		scanf("%d", &selection);
+	}
+	char *to_scan_file_name = strdup(files[selection]->d_name);
+	return to_scan_file_name;
+	for(int i = 0; i < current_file; i++){
+		//closedir(files[i]);
+	}
+
 	closedir(d);
 	return EXIT_SUCCESS;
 }
